c1b6563b63cd846ebaf304a99cf7f261e9bc6785
Devorah L Newman <denewman@cisco.com>
2016-09-19 21:02:35 +1000

    mdtconf

--- sqlite-api.py
+++ sqlite-api.py
@@ -741,21 +741,23 @@ class subscriptionRouterLink(Resource):
                 _routerPort = db.execute(
                     'SELECT routerPort FROM router WHERE routerName=?', (router,)).fetchone()[0]
 
-                conf = mdtconf.Mdtconf(_routerAddress, _routerUsername, _routerPassword, _routerPort,
-                                       _accessProtocol, _destinationGroupName, _addressFamily, _destinationGroupAddress,
-                                       _destinationGroupPort, _sensorName, _sensorPath, _subscriptionName, _subscriptionId,
-                                       _subscriptionInterval)
+#                conf = mdtconf.Mdtconf(_routerAddress, _routerUsername, _routerPassword, _routerPort,
+#                                       _accessProtocol, _destinationGroupName, _addressFamily, _destinationGroupAddress,
+#                                       _destinationGroupPort, _sensorName, _sensorPath, _subscriptionName, _subscriptionId,
+#                                       _subscriptionInterval)
+
+                conf = mdtconf.Mdtconf('64.104.255.10', 'rmitproject', 'r@mot@supp@rt', 5001, 'ssh', 'Dgroup1', 'ipv4', '172.30.8.4', 5432, 'SGroup1', 'Cisco-IOS-XR-infra-statsd-oper:infra-statistics/interfaces/interface/latest/generic-counters', 'Sub1', 5, 3000)
 
                 print (conf.push_conf() == 0)
                 if len(data) is 0:
                     db.commit()
 
-#                print (_routerAddress, _routerUsername,  _routerPassword, _routerPort,
-#                                       _accessProtocol, _destinationGroupName, _addressFamily, _destinationGroupAddress,
-#                                       _destinationGroupPort, _sensorName, _sensorPath, _subscriptionName, _subscriptionId,
-#                                       _subscriptionInterval)
+                print (_routerAddress, _routerUsername,  _routerPassword, _routerPort,
+                                       _accessProtocol, _destinationGroupName, _addressFamily, _destinationGroupAddress,
+                                       _destinationGroupPort, _sensorName, _sensorPath, _subscriptionName, _subscriptionId,
+                                       _subscriptionInterval)
 
-                    router_list.append(router)
+                router_list.append(router)
 
         except Exception as e:
             return {'error': str(e)}
@@ -867,18 +869,23 @@ class singleSubscriptionRouterLink(Resource):
                 _routerUsername = router[1]
                 _routerPassword = router[2]
                 _routerPort = router[3]
-#                print (_routerAddress, _routerUsername, _routerPassword, _routerPort,
-#                                       _accessProtocol, _destinationGroupName, _addressFamily, _destinationGroupAddress,
-#                                       _destinationGroupPort, _sensorName, _sensorPath, _subscriptionName, _subscriptionId,
-#                                       _subscriptionInterval)
+                print (_routerAddress, _routerUsername, _routerPassword, _routerPort,
+                                       _accessProtocol, _destinationGroupName, _addressFamily, _destinationGroupAddress,
+                                       _destinationGroupPort, _sensorName, _sensorPath, _subscriptionName, _subscriptionId,
+                                       _subscriptionInterval)
 
-            conf = mdtconf.Mdtconf(_routerAddress, _routerUsername, _routerPassword, _routerPort,
-                                   _accessProtocol, _destinationGroupName, _addressFamily, _destinationGroupAddress,
-                                  _destinationGroupPort, _sensorName, _sensorPath, _subscriptionName, _subscriptionId,
-                                  _subscriptionInterval)
-            if conf.del_conf() == 0:
-                db.execute('DELETE FROM linkSubscriptionRouter WHERE linkId=?', (linkId,))
-                db.commit()
+                conf = mdtconf.Mdtconf('64.104.255.10', 'rmitproject', 'r@mot@supp@rt', 5001, 'ssh', 'Dgroup1', 'ipv4',
+                                   '172.30.8.4', 5432, 'SGroup1',
+                                   'Cisco-IOS-XR-infra-statsd-oper:infra-statistics/interfaces/interface/latest/generic-counters',
+                                   'Sub1', 5, 3000)
+
+#               conf = mdtconf.Mdtconf(_routerAddress, _routerUsername, _routerPassword, _routerPort,
+#                                   _accessProtocol, _destinationGroupName, _addressFamily, _destinationGroupAddress,
+#                                  _destinationGroupPort, _sensorName, _sensorPath, _subscriptionName, _subscriptionId,
+#                                  _subscriptionInterval)
+#               print conf.del_conf()
+            db.execute('DELETE FROM linkSubscriptionRouter WHERE linkId=?', (linkId,))
+            db.commit()
 
             return {'Status Code': '200'}
 

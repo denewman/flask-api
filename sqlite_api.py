53acb78e189ef5a2ce9c4bcbb6cbbee708019421
Devorah L Newman <denewman@cisco.com>
2016-11-03 18:27:07 +1100

    test organizing code

--- sqlite_api.py
+++ sqlite_api.py
@@ -15,6 +15,7 @@ from plotly.tools import FigureFactory as FF
 import plotly.graph_objs as go
 import plotly.tools as tls
 import numpy as np
+import subscription
 
 tls.set_credentials_file(username='rmit-s3471293-Zhongyang-Wang', api_key='gw5mcyvrf8')
 
@@ -131,75 +132,18 @@ class visualization(Resource):
 
 class subscription(Resource):
     def post(self):
-        try:
-            # Parse the arguments
-            parser = reqparse.RequestParser()
-            parser.add_argument('subscriptionId', type=int, help='Subscription ID to create subscription')
-            parser.add_argument('subscriptionName', type=str, help='Subscription name to create subscription')
-            parser.add_argument('destinationGroupName', type=str, help='Destination group to create subscription')
-            parser.add_argument('sensorName', type=str, help='Sensor Name to create subscription')
-            parser.add_argument('subscriptionInterval', type=float, help='Interval for the new subscription')
-            args = parser.parse_args()
+        db = get_db()
+        return subscription.post(db)
 
-            _subscriptionId = args['subscriptionId']
-            _subscriptionName = args['subscriptionName']
-            _destinationGroupName = args['destinationGroupName']
-            _sensorName = args['sensorName']
-            _subscriptionInterval = args['subscriptionInterval']
-
-            db = get_db()
-            db.execute('PRAGMA foreign_keys=ON')
-            cursor = db.execute(
-                'INSERT INTO subscription (subscriptionId, subscriptionName, destinationGroupName, sensorName, subscriptionInterval) VALUES (?, ?, ?, ?, ?)',
-                [_subscriptionId, _subscriptionName, _destinationGroupName, _sensorName, _subscriptionInterval])
-            data = cursor.fetchall()
-
-            if len(data) is 0:
-                db.commit()
-                return {'subscription': {'subscriptionId': _subscriptionId,
-                                         'subscriptionName': _subscriptionName,
-                                         'destinationGroupName': _destinationGroupName,
-                                         'sensorName': _sensorName,
-                                         'subscriptionInterval': _subscriptionInterval}}
-            else:
-                return {'Status Code': '1000', 'Message': str(data[0])}
-
-        except Exception as e:
-            return {'error': str(e)}
 
     def get(self):
-        try:
-            db = get_db()
-            cursor = db.execute(
-                'SELECT subscriptionId, subscriptionName, destinationGroupName, sensorName, subscriptionInterval FROM subscription ORDER BY subscriptionName DESC')
-            data = cursor.fetchall()
+        db = get_db()
+        return subscription.get(db)
 
-            subscription_list = []
-            for subscription in data:
-                i = {
-                    'subscriptionId': subscription[0],
-                    'subscriptionName': subscription[1],
-                    'destinationGroupName': subscription[2],
-                    'sensorName': subscription[3],
-                    'subscriptionInterval': subscription[4]
-                }
-                subscription_list.append(i)
-
-            return {'Status Code': '200', 'subscription': subscription_list}
-
-        except Exception as e:
-            return {'error': str(e)}
 
     def delete(self):
-        try:
-            db = get_db()
-            db.execute('PRAGMA foreign_keys=ON')
-            db.execute('DELETE FROM subscription')
-            db.commit()
-            return {'Status Code': '200'}
-
-        except Exception as e:
-            return {'error': str(e)}
+        db = get_db()
+        return subscription.delete(db)
 
 
 class singleSubscription(Resource):
--- subscription.py
+++ subscription.py
@@ -1,108 +1,70 @@
-from flask_restful import Resource, reqparse
-from sqlite_api import get_db
+from flask_restful import reqparse
 
 
-class subscription(Resource):
-    def post(self):
-        try:
-            # Parse the arguments
-            parser = reqparse.RequestParser()
-            parser.add_argument('subscriptionId', type=int, help='Subscription ID to create subscription')
-            parser.add_argument('subscriptionName', type=str, help='Subscription name to create subscription')
-            parser.add_argument('destinationGroupName', type=str, help='Destination group to create subscription')
-            parser.add_argument('sensorName', type=str, help='Sensor Name to create subscription')
-            parser.add_argument('subscriptionInterval', type=float, help='Interval for the new subscription')
-            args = parser.parse_args()
+def post(db):
+    try:
+        # Parse the arguments
+        parser = reqparse.RequestParser()
+        parser.add_argument('subscriptionId', type=int, help='Subscription ID to create subscription')
+        parser.add_argument('subscriptionName', type=str, help='Subscription name to create subscription')
+        parser.add_argument('destinationGroupName', type=str, help='Destination group to create subscription')
+        parser.add_argument('sensorName', type=str, help='Sensor Name to create subscription')
+        parser.add_argument('subscriptionInterval', type=float, help='Interval for the new subscription')
+        args = parser.parse_args()
 
-            _subscriptionId = args['subscriptionId']
-            _subscriptionName = args['subscriptionName']
-            _destinationGroupName = args['destinationGroupName']
-            _sensorName = args['sensorName']
-            _subscriptionInterval = args['subscriptionInterval']
+        _subscriptionId = args['subscriptionId']
+        _subscriptionName = args['subscriptionName']
+        _destinationGroupName = args['destinationGroupName']
+        _sensorName = args['sensorName']
+        _subscriptionInterval = args['subscriptionInterval']
 
-            db = get_db()
-            db.execute('PRAGMA foreign_keys=ON')
-            cursor = db.execute(
-                'INSERT INTO subscription (subscriptionId, subscriptionName, destinationGroupName, sensorName, subscriptionInterval) VALUES (?, ?, ?, ?, ?)',
-                [_subscriptionId, _subscriptionName, _destinationGroupName, _sensorName, _subscriptionInterval])
-            data = cursor.fetchall()
+        db.execute('PRAGMA foreign_keys=ON')
+        cursor = db.execute(
+            'INSERT INTO subscription (subscriptionId, subscriptionName, destinationGroupName, sensorName, subscriptionInterval) VALUES (?, ?, ?, ?, ?)',
+            [_subscriptionId, _subscriptionName, _destinationGroupName, _sensorName, _subscriptionInterval])
+        data = cursor.fetchall()
 
-            if len(data) is 0:
-                db.commit()
-                return {'subscription': {'subscriptionId': _subscriptionId,
-                                         'subscriptionName': _subscriptionName,
-                                         'destinationGroupName': _destinationGroupName,
-                                         'sensorName': _sensorName,
-                                         'subscriptionInterval': _subscriptionInterval}}
-            else:
-                return {'Status Code': '1000', 'Message': str(data[0])}
+        if len(data) is 0:
+            db.commit()
+            return {'subscription': {'subscriptionId': _subscriptionId,
+                                     'subscriptionName': _subscriptionName,
+                                     'destinationGroupName': _destinationGroupName,
+                                     'sensorName': _sensorName,
+                                    'subscriptionInterval': _subscriptionInterval}}
+        else:
+            return {'Status Code': '400', 'Message': str(data[0])}
 
-        except Exception as e:
-            return {'error': str(e)}
+    except Exception as e:
+        return {'error': str(e)}
 
-    def get(self):
-        try:
-            db = get_db()
-            cursor = db.execute(
-                'SELECT subscriptionId, subscriptionName, destinationGroupName, sensorName, subscriptionInterval FROM subscription ORDER BY subscriptionName DESC')
-            data = cursor.fetchall()
+def get(db):
+    try:
+        cursor = db.execute(
+            'SELECT subscriptionId, subscriptionName, destinationGroupName, sensorName, subscriptionInterval FROM subscription ORDER BY subscriptionName DESC')
+        data = cursor.fetchall()
 
-            subscription_list = []
-            for subscription in data:
-                i = {
+        subscription_list = []
+        for subscription in data:
+            i = {
                     'subscriptionId': subscription[0],
                     'subscriptionName': subscription[1],
                     'destinationGroupName': subscription[2],
                     'sensorName': subscription[3],
                     'subscriptionInterval': subscription[4]
-                }
-                subscription_list.append(i)
-
-            return {'Status Code': '200', 'subscription': subscription_list}
-
-        except Exception as e:
-            return {'error': str(e)}
-
-    def delete(self):
-        try:
-            db = get_db()
-            db.execute('PRAGMA foreign_keys=ON')
-            db.execute('DELETE FROM subscription')
-            db.commit()
-            return {'Status Code': '200'}
-
-        except Exception as e:
-            return {'error': str(e)}
-
-
-class singleSubscription(Resource):
-    def get(self, subscriptionName):
-        try:
-            db = get_db()
-            query = db.execute(
-                'SELECT subscriptionId, subscriptionName, destinationGroupName, sensorName, subscriptionInterval FROM subscription WHERE subscriptionName =?',
-                (subscriptionName,))
-            subscription = query.fetchone()
-            data = {
-                'subscriptionId': subscription[0],
-                'subscriptionName': subscription[1],
-                'destinationGroupName': subscription[2],
-                'sensorName': subscription[3],
-                'subscriptionInterval': subscription[4]
             }
+            subscription_list.append(i)
 
-            return {'Status Code': '200', 'data': data}
+        return {'Status Code': '200', 'subscription': subscription_list}
 
-        except Exception as e:
-            return {'error': str(e)}
+    except Exception as e:
+        return {'error': str(e)}
 
-    def delete(self, subscriptionName):
-        try:
-            db = get_db()
-            db.execute('PRAGMA foreign_keys=ON')
-            db.execute('DELETE FROM subscription WHERE subscriptionName=?', (subscriptionName,))
-            db.commit()
-            return {'statusCode': '200'}
+def delete(db):
+    try:
+        db.execute('PRAGMA foreign_keys=ON')
+        db.execute('DELETE FROM subscription')
+        db.commit()
+        return {'Status Code': '200'}
 
-        except Exception as e:
-            return {'error': str(e)}
+    except Exception as e:
+        return {'error': str(e)}
